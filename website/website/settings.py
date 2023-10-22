9d2cbdc8a9516a22b48f0293bdc6b7ec800e7a5a
M1CAELAN <michaelklinkov@gmail.com>
2023-10-23 01:19:33 +0300

    database

--- website/website/settings.py
+++ website/website/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'website',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '26.168.4.104',
+        'PORT': '5432'
     }
 }
 
