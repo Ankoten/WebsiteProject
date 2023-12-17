01c9edbf55b3b44bb3c8b74fa6572e718bf9900f
M1CAELAN <michaelklinkov@gmail.com>
2023-12-17 22:32:22 +0300

    документация

--- website/app_site/tests.py
+++ website/app_site/tests.py
@@ -1,10 +1,13 @@
 from django.urls import reverse
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from app_site.models import Category
 
 class CategoryTests(APITestCase):
+    def setUp(self):
+        Category.objects.create(title="однушки")
     def test_category_list(self):
         response = self.client.get(reverse("categories"))
-        print(response)
-
+        print(response.json())
+        self.assertTrue({'id': 1, 'title': 'однушки'} in response.json())
 # Create your tests here.
