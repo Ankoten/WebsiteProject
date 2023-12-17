2e90cd197d279c5e4cd11c88c99668f5e4deede6
Ankoten <ankotenlife@gmail.com>
2023-12-17 08:43:08 +0300

    Commit

--- website/app_site/models.py
+++ website/app_site/models.py
@@ -2,14 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from pathlib import Path
 from django.urls import reverse
-#class UserModel(models.Model):
-    #name = models.CharField(max_length=100, null=False)
-    #surname = models.CharField(max_length=100, null=False)
-    #telephone = models.CharField(max_length=16, verbose_name='Телефон', blank=True)
-    #password = models.CharField(max_length=100, null=False)
-
-    #def str(self):
-        #return {self.name, self.surname}
 
 class AdvertModel(models.Model):
     advert = models.CharField(max_length=100, null=False)
@@ -19,7 +11,6 @@ class AdvertModel(models.Model):
     description = models.TextField()
     payment_hcs = models.BooleanField(default=False)
     deposit = models.PositiveBigIntegerField(default=0, null=False)
-    #user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     total_area = models.FloatField(max_length=1000, null=False)
     kitchen_area = models.FloatField(max_length=100, null=False)
     living_area = models.FloatField(max_length=100, null=False)
