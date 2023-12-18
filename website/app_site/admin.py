33283b33f4b0e160072c3bcbec4833ec4c266369
M1CAELAN <michaelklinkov@gmail.com>
2023-12-18 08:25:27 +0300

    test

--- website/app_site/admin.py
+++ website/app_site/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Advert, User, Category, AppUser
+from .models import Advert, User, Category
 
 admin.site.register(Advert)
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(AppUser)
--- website/app_site/views.py
+++ website/app_site/views.py
@@ -3,7 +3,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from .models import Advert, Category, User, AppUser
+from .models import Advert, Category, User
 from rest_framework import status, viewsets, permissions
 from .serializers import AdvertSerializer, CategorySerializer, UserSerializer, UserRegistrationSerializer, \
     UserLoginSerializer
